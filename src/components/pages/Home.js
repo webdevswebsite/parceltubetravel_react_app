49a272c3ceebf42a26dcbe88bc07674ae8c8363a
olu-martins <mayowasamuel86@gmail.com>
2023-10-13 18:21:15 +0100

    build

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -2,7 +2,7 @@ import React, { Component, Fragment } from 'react';
 import MetaTags from "react-meta-tags";
 import Header from '../layouts/Header';
 import Footer from '../layouts/Footer';
-import Content from '../sections/home/Content';
+// import Content from '../sections/home/Content';
 import Maintenance from './Maintenance';
 
 
